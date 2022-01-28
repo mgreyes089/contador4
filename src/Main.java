87969f6eae689ba8da4586a094376bef4c8ce3c2
mgreyes089 <98517687+mgreyes089@users.noreply.github.com>
2022-01-28 12:19:05 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escriba su nombre ");
+        int number1 = sc.nextInt();
+
+        in.nextLine();
+
+
+
+
+
+
+
+    }
+}
+
+
+
